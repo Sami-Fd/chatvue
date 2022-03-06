d797e9b188edcf34d37dc5f2dda40aab74be1e73
Sami <67370636+Sami-Fd@users.noreply.github.com>
2022-03-06 17:13:26 +0100

    firebase config

--- fireChat/src/firebase.js
+++ fireChat/src/firebase.js
@@ -2,13 +2,14 @@ import * as firebase from "firebase";
 import "firebase/firestore";
 
 let config = {
-  apiKey: "AIzaSyAp1OdrrVLedbJPgcEZ87951Wjqt_18YE4",
-  authDomain: "firechat-7a8d9.firebaseapp.com",
-  databaseURL: "https://firechat-7a8d9-default-rtdb.firebaseio.com",
-  projectId: "firechat-7a8d9",
-  storageBucket: "firechat-7a8d9.appspot.com",
-  messagingSenderId: "448283085253",
-  appId: "1:448283085253:web:e107e5e5f33822bfaae3ac"
+  // your firbase account config
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 firebase.initializeApp(config);
